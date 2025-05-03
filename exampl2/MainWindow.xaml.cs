17b43b3b208b3b6aade1b418212b7480662d0a21
Kondratenko Mikhailo <mike.kondr@gmail.com>
2025-05-03 20:00:08 +0300

    example2

---
+++ exampl2/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+
+namespace example2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void customControl_Click(object sender, RoutedEventArgs e)
+        {
+            txtBlock.Text = "You have just click your custom control";
+        }
+    }
+}
---
+++ exampl2/MyCustomControl.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+
+namespace example2
+{
+    public class MyCustomControl : Button
+    {
+        static MyCustomControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MyCustomControl), new FrameworkPropertyMetadata(typeof(MyCustomControl)));
+        }
+    }
+}
